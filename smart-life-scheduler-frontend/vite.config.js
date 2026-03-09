5acf54374dedeb0488980980e03dc05bc0314529
thbny <pehlajnettu@gmail.com>
2026-03-09 12:16:35 +0530

    Fix vite chunk limit

--- smart-life-scheduler-frontend/vite.config.js
+++ smart-life-scheduler-frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increased limit to hide the 500kb warning for the 'index-*.js' chunk
+  },
 })
